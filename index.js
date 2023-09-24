8b5363029ae87a8ad2049527213be3e55cc221fb
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-24 12:19:29 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+num1 = 31
+num2 = 2
+multiply = num1 * num2
+console.log(multiply)
+
+
+
+Math.floor(Math.random() * 10) + 1
+
+num3 = 14
+num4 = 10
+mod = num3 % num4
+console.log(mod)
+
+numbers = [5, 10, 15, 20, 18]
+
+max_value = max(numbers)
+console.log(max_value)
